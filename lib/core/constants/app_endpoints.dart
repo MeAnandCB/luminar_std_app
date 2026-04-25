10c3974af2758ebc6ce45ea44d08036d5404ce61
MeAnandCB <anandcb55@gmail.com>
2026-04-25 09:57:26 +0530

    chore: update production base and websocket URLs in GlobalLinks

--- lib/core/constants/app_endpoints.dart
+++ lib/core/constants/app_endpoints.dart
@@ -83,8 +83,7 @@ class AppEndpoints {
 }
 
 class GlobalLinks {
-  static const String baseUrl = 'https://api.crm.dev.luminartechnohub.com';
+  static const String baseUrl = 'https://api.crm.luminartechnohub.com';
   //static const String baseUrl = 'http://192.168.1.41:8000';
-  static const String websocketUrl =
-      'wss://api.crm.dev.luminartechnohub.com/ws/';
+  static const String websocketUrl = 'wss://api.crm.luminartechnohub.com/ws/';
 }
